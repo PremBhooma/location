5ca0c77ece58477fa628a4f06ab36836bc4c9a72
PremBhooma <bhoomasagar1213@gmail.com>
2024-02-07 00:43:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [userLocation, setUserLocation] = useState();
+
+
+  useEffect(() => {
+    const successCallBack = async (position) => {
+      try {
+        let response = await fetch(
+          `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+        );
+        let data = await response.json();
+        console.log("locationData:", data);
+        setUserLocation(data);
+      } catch (error) {
+        console.error("Error fetching location data:", error);
+      }
+    };
+
+    const errorCallBack = (error) => {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          console.log("Please allow access to location");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.log("Location Information unavailable");
+          break;
+        case error.TIMEOUT:
+          console.log("The request to get user location timed out");
+      }
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallBack, errorCallBack);
+    } else {
+      console.log("The browser does not support geolocation");
+    }
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your Current Location: {userLocation?.display_name}</h1>
     </div>
   );
 }
